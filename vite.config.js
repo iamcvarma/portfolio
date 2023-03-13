a7def0c80306bc1ae1cb25d9b81ccfd4da8a9cc4
Chaitanya <iamcvers@gmail.com>
2023-03-13 16:01:51 +0530

    added base url for assets in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     svgr({
       svgoConfig: {
         plugins: [{ removeViewBox: false }],
@@ -15,4 +16,5 @@ export default defineConfig({
       },
     }),
   ],
-})
+  base: "/portfolio/",
+});
